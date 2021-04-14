faed33c22ab1ae7db4c71b570fcec0ef11378c00
Ivan Ilchenko <ivan.ilchen2002@gmail.com>
2021-04-14 22:23:37 +0300

    Class declaration

--- mainPart.cpp
+++ mainPart.cpp
@@ -2,4 +2,21 @@
 
 using namespace std;
 
+struct point {double x, y;};
 
+class polygon
+{
+private:
+    point *A;
+    int N;
+    // прочие скрытые переменные и функции
+public:
+    polygon(): N(0), A(NULL) {}; // DONE
+    polygon(int n);
+    polygon(const polygon&);
+    ~polygon();
+    polygon& operator=(const polygon&);
+    point& operator[] (int);
+    friend ostream& operator<<(ostream&, const polygon&);
+    polygon operator*(const polygon&);
+};
