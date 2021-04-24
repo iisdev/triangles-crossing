063f43867d514a9719b0e54882290693502b73c9
Ivan Ilchenko <ivan.ilchen2002@gmail.com>
2021-04-24 16:14:39 +0300

    Create lines

--- mainPart.cpp
+++ mainPart.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 struct point {double x, y;};
+struct line {double x1, y1, x2, y2;};
 
 class polygon
 {
 private:
     point *A;
+    line *L;
     int N;
     // прочие скрытые переменные и функции
 public:
@@ -19,7 +21,11 @@ public:
     point& operator[] (int); // DONE
     friend ostream& operator<<(ostream&, const polygon&); // DONE
     polygon operator*(const polygon&);
+
+    //new methods
     int len() {return N;} // DONE
+    void setLines(); // make lines by points
+    void showLines(); // show all lines
 };
 
 polygon::polygon(int n) // class constr
@@ -54,6 +60,7 @@ polygon::polygon(polygon& obj, int n) // copy constr
 polygon::~polygon() // DELETE POLYGON
 {
     delete[] A;
+    delete[] L;
 }
 
 point& polygon::operator[](int n) // [] check
@@ -93,12 +100,35 @@ polygon& polygon::operator=( polygon& obj) // = copy func
     return *this;
 }
 
+//new methods
+void polygon::setLines()
+{
+    L = new line[N];
+
+    for (int i = 0; i < (N-1); i++)
+    {
+        L[i].x1 = A[i].x;
+        L[i].y1 = A[i].y;
+
+        L[i].x2 = A[i+1].x;
+        L[i].y2 = A[i+1].y;
+    }
+    //last line
+    L[N-1].x1 = A[N-1].x;
+    L[N-1].y1 = A[N-1].y;
+    L[N-1].x2 = A[0].x;
+    L[N-1].y2 = A[0].y;
+}
+void polygon::showLines()
+{
+    for(int i = 0; i < N; i++)
+        cout << "Line: " << L[i].x1 << ' ' << L[i].y1 << ' ' << L[i].x2 << ' ' << L[i].y2 << endl;
+}
+
 
 int main()
 {
     polygon obj = polygon(3);
-    polygon obj1;
-    obj1 = obj;
-
-    cout << obj1;
+    obj.setLines();
+    obj.showLines();
 }
